148531af421c316c5047d6cea52544502163eb9e
Mike McNeil <mike@balderdash.co>
2015-02-13 15:51:38 -0800

    Support multiple zip sources

--- machines/zip.js
+++ machines/zip.js
@@ -3,14 +3,14 @@ module.exports = {
   description: 'Zip up the specified source file or directory and write a .zip file to disk.',
   extendedDescription: '',
   inputs: {
-    source: {
-      example: '/Users/mikermcneil/.tmp/',
-      description: 'The path (relative or absolute) to the file or directory to zip.',
-      extendedDescription: 'If a relative path is provided, it will be resolved to an absolute path using the current working directory.',
+    sources: {
+      example: ['/Users/mikermcneil/foo/api/'],
+      description: 'The paths (relative or absolute) to the files or directories to zip.',
+      extendedDescription: 'If a relative paths are provided, they will be resolved to an absolute path using the current working directory.',
       required: true
     },
     destination: {
-      example: '/Users/mikermcneil/tmp-contents.zip',
+      example: '/Users/mikermcneil/my-app.zip',
       // TODO: default to using `inputs.source` + ".zip" and just creating the output archive in the same dir that the source lives in (or i.e. the first source in the list, or something)
       description: 'The path (relative or absolute) where the .zip file should be written.',
       extendedDescription: 'If a relative path is provided, it will be resolved to an absolute path using the current working directory.',
@@ -33,9 +33,12 @@ module.exports = {
 
     var path = require('path');
     var fs = require('fs');
+    var _ = require('lodash');
     var Archiver = require('archiver');
 
-    var srcPath = path.resolve(inputs.source);
+    var srcPaths = _.map(inputs.sources, function (sourcePath){
+      return path.resolve(sourcePath);
+    });
     var zipFileDestination = path.resolve(inputs.destination);
     // console.log('from %s to %s', srcPath, zipFileDestination);
 
@@ -56,18 +59,17 @@ module.exports = {
 
     archive.pipe(outputStream);
 
-    // Get (1) srcPath's parent and (2) the relative path to our srcPath from its parent
-    var srcParent = path.resolve(srcPath, '..');
-    var srcRelative = path.relative(srcParent, srcPath);
+    archive.bulk(_.map(srcPaths, function convertPathIntoArchiverBulkInstr(srcPath){
+      // Get (1) srcPath's parent and (2) the relative path to our srcPath from its parent
+      var srcParent = path.resolve(srcPath, '..');
+      var srcRelative = path.relative(srcParent, srcPath);
 
-    // console.log('srcParent',srcParent);
-    // console.log('srcRelative',srcRelative);
-    // console.log('dest',zipFileDestination);
-    // console.log('srces: ',[ srcRelative, path.join(srcRelative, '**') ]);
-
-    archive.bulk([
-      { src: [ srcRelative, path.join(srcRelative, '**') ], cwd: srcParent, expand: true }
-    ]);
+      // console.log('srcParent',srcParent);
+      // console.log('srcRelative',srcRelative);
+      // console.log('dest',zipFileDestination);
+      // console.log('srces: ',[ srcRelative, path.join(srcRelative, '**') ]);
+      return { src: [ srcRelative, path.join(srcRelative, '**') ], cwd: srcParent, expand: true };
+    }));
 
     archive.finalize();
 
