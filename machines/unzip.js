2e156c53df3f4d6c3f5342120be98af8d0f8fba7
Mike McNeil <mike@balderdash.co>
2015-02-13 20:54:38 -0800

    Ensure directory exists in zip and unzip

--- machines/unzip.js
+++ machines/unzip.js
@@ -31,16 +31,30 @@ module.exports = {
     var path = require('path');
     var fs = require('fs');
     var unzip = require('unzip');
+    var Filesystem = require('machinepack-fs');
 
     var sourceArchive = path.resolve(inputs.source);
     var destinationDir = path.resolve(inputs.destination);
 
-    var srcStream = fs.createReadStream(sourceArchive);
-    var drainStream = unzip.Extract({ path: destinationDir });
-    drainStream.once('close', function (){ return exits.success(); });
-    drainStream.once('error', function (err){ return exits.error(err); });
-    srcStream.once('error', function (err){ return exits.error(err); });
-    srcStream.pipe(drainStream);
+    // Ensure the parent directory exists.
+    Filesystem.ensureDir({
+      dir: destinationDir
+    }).exec({
+
+      // An unexpected error occurred.
+      error: exits.error,
+
+      // OK.
+      success: function() {
+
+        var srcStream = fs.createReadStream(sourceArchive);
+        var drainStream = unzip.Extract({ path: destinationDir });
+        drainStream.once('close', function (){ return exits.success(); });
+        drainStream.once('error', function (err){ return exits.error(err); });
+        srcStream.once('error', function (err){ return exits.error(err); });
+        srcStream.pipe(drainStream);
+      }
+    });
 
   }
 
--- machines/zip.js
+++ machines/zip.js
@@ -35,44 +35,53 @@ module.exports = {
     var fs = require('fs');
     var _ = require('lodash');
     var Archiver = require('archiver');
+    var Filesystem = require('machinepack-fs');
 
     var srcPaths = _.map(inputs.sources, function (sourcePath){
       return path.resolve(sourcePath);
     });
     var zipFileDestination = path.resolve(inputs.destination);
-    // console.log('from %s to %s', srcPath, zipFileDestination);
+    var zipFileParentPath = path.resolve(zipFileDestination, '..');
 
-    var archive = Archiver('zip');
+    // Ensure the parent directory exists.
+    Filesystem.ensureDir({
+      dir: zipFileParentPath
+    }).exec({
 
-    var outputStream = fs.createWriteStream(zipFileDestination);
-    outputStream.once('close', function() {
-      return exits.success({
-        bytesWritten: archive.pointer()
-      });
+      // An unexpected error occurred.
+      error: exits.error,
+
+      // OK.
+      success: function() {
+
+        var archive = Archiver('zip');
+
+        var outputStream = fs.createWriteStream(zipFileDestination);
+        outputStream.once('close', function() {
+          return exits.success({
+            bytesWritten: archive.pointer()
+          });
+        });
+        outputStream.once('error', function (err) {
+          return exits.error(err);
+        });
+        archive.once('error', function (err) {
+          return exits.error(err);
+        });
+
+        archive.pipe(outputStream);
+
+        archive.bulk(_.map(srcPaths, function convertPathIntoArchiverBulkInstr(srcPath){
+          // Get (1) srcPath's parent and (2) the relative path to our srcPath from its parent
+          var srcParent = path.resolve(srcPath, '..');
+          var srcRelative = path.relative(srcParent, srcPath);
+          return { src: [ srcRelative, path.join(srcRelative, '**') ], cwd: srcParent, expand: true };
+        }));
+
+        archive.finalize();
+
+      }
     });
-    outputStream.once('error', function (err) {
-      return exits.error(err);
-    });
-    archive.once('error', function (err) {
-      return exits.error(err);
-    });
-
-    archive.pipe(outputStream);
-
-    archive.bulk(_.map(srcPaths, function convertPathIntoArchiverBulkInstr(srcPath){
-      // Get (1) srcPath's parent and (2) the relative path to our srcPath from its parent
-      var srcParent = path.resolve(srcPath, '..');
-      var srcRelative = path.relative(srcParent, srcPath);
-
-      // console.log('srcParent',srcParent);
-      // console.log('srcRelative',srcRelative);
-      // console.log('dest',zipFileDestination);
-      // console.log('srces: ',[ srcRelative, path.join(srcRelative, '**') ]);
-      return { src: [ srcRelative, path.join(srcRelative, '**') ], cwd: srcParent, expand: true };
-    }));
-
-    archive.finalize();
-
 
   },
 
